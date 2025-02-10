784ed3138cdb6b546d0fe07871bfce0bc21bffb8
rajatkinlekar <kinlekarrajat@gmail.com>
2025-02-11 02:50:18 +0530

    Added the Concepts class

---
+++ src/main/java/org/example/Concepts.java
@@ -0,0 +1,34 @@
+package org.example;
+
+public class Concepts {
+
+    public Node mergeTwoLists(Node head1, Node head2) {
+        Node t1 = head1;
+        Node t2 = head2;
+
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+
+        while (t1 != null && t2 != null) {
+            if (t1.getValue() <= t2.getValue()) {
+                temp.setNext(t1);
+                temp = t1;
+                t1 = t1.getNext();
+            } else {
+                temp.setNext(t2);
+                temp = t2;
+                t2 = t2.getNext();
+            }
+        }
+
+        // L2 > L1
+        if (t1 == null) {
+            temp.setNext(t2);
+        } else {
+            temp.setNext(t1);
+        }
+
+        return dummy.getNext();
+    }
+
+}
